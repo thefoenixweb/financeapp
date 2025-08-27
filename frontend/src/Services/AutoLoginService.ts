b6e2be8d50fa7bb2a88ee11b8fb5a71bf1795160
Tshepiso Molefi <tshepiso.molefi@yahoo.com>
2025-08-27 19:14:52 +0200

    Backend connect

--- frontend/src/Services/AutoLoginService.ts
+++ frontend/src/Services/AutoLoginService.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BACKEND_URL = 'http://localhost:5167/api/account';
+const BACKEND_URL = 'https://financeapp-fvemb9b0fhgdhyce.southafricanorth-01.azurewebsites.net/account';
 
 export interface AutoLoginResponse {
   userName: string;
